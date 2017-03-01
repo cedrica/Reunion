e274281d9c6ae9d4789dcdb1aba7220d5f8e82e6
Cedric Leumaleu <ca.leumaleu@project-immobilien.com>
2017-03-01 09:18:33 +0100

    egal

--- src/main/java/com/reunion/bean/MembreBean.java
+++ src/main/java/com/reunion/bean/MembreBean.java
@@ -1,9 +1,14 @@
 package com.reunion.bean;
 
 import java.io.Serializable;
+import java.util.HashMap;
 import java.util.List;
 
 import javax.enterprise.context.ConversationScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.component.UIOutput;
+import javax.faces.context.FacesContext;
+import javax.faces.event.AjaxBehaviorEvent;
 import javax.inject.Inject;
 import javax.inject.Named;
 
@@ -11,6 +16,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.reunion.business.MembreService;
+import com.reunion.common.Helper;
 import com.reunion.common.Pages;
 import com.reunion.helper.ModelInitializer;
 import com.reunion.model.Membre;
@@ -19,18 +25,23 @@ import com.reunion.model.Membre;
 @ConversationScoped
 public class MembreBean extends BasicBean implements Serializable {
 
-
 	@Inject
 	MembreService membreService;
-	
+
 	private List<Membre> allMembers;
 	private Membre membre;
 	private static final long serialVersionUID = 1L;
 	private final Logger LOG = LoggerFactory.getLogger(this.getClass());
-	
+	private boolean inputsValid;
+	private HashMap<String, String> map = null;
 
-	
 	public void init() {
+		if (map == null) {
+			map = new HashMap<>();
+			map.put("email", "");
+			map.put("nom", "");
+			map.put("prenom", "");
+		}
 		membreService.startConversation();
 		membre = ModelInitializer.initMembre();
 	}
@@ -43,12 +54,30 @@ public class MembreBean extends BasicBean implements Serializable {
 		this.membre = membre;
 	}
 
+	public void entreLeNom(AjaxBehaviorEvent e) {
+		String id = e.getComponent().getId();
+		String valeur = (String) ((UIOutput) e.getSource()).getValue();
+		map.put(id, valeur);
+		if (!map.get("nom").isEmpty() && !map.get("prenom").isEmpty() && !map.get("email").isEmpty()) {
+			setInputsValid(true);
+		}
+	}
+
 	public String save() {
 		Membre m = membreService.createMembre(membre);
 		if (m != null) {
 			LOG.debug("Le membre " + m.getNom() + " a été creer");
 		}
 		return Pages.MEMBRES;
+	}
+
+	public boolean isInputsValid() {
+		return this.inputsValid;
+	}
+
+	public void setInputsValid(boolean inputsValid) {
+		System.out.println("inputs Valid :" + inputsValid);
+		this.inputsValid = inputsValid;
 	}
 
 	public List<Membre> getAllMembers() {
@@ -59,4 +88,5 @@ public class MembreBean extends BasicBean implements Serializable {
 	public void setAllMembers(List<Membre> allMembers) {
 		this.allMembers = allMembers;
 	}
+
 }
--- src/main/java/com/reunion/common/Helper.java
+++ src/main/java/com/reunion/common/Helper.java
@@ -1,10 +1,37 @@
 package com.reunion.common;
 
+import java.util.Iterator;
+
 import javax.faces.component.UIComponent;
 import javax.faces.component.UIInput;
+import javax.faces.component.UIViewRoot;
+import javax.faces.context.FacesContext;
 
 public class Helper {
 
+	public static UIComponent findComponent(String id) {
+		FacesContext context = FacesContext.getCurrentInstance(); 
+	    UIViewRoot root = context.getViewRoot();
+	    if (id.equals(root.getId()))
+	        return root;
+	    
+	      UIComponent kid = null;
+	      UIComponent result = null;
+	      Iterator kids = root.getFacetsAndChildren();
+	      while (kids.hasNext() && (result == null)) {
+	        kid = (UIComponent) kids.next();
+	        if (id.equals(kid.getId())) {
+	          result = kid;
+	          break;
+	        }
+	        result = findComponent(id);
+	        if (result != null) {
+	          break;
+	        }
+	      }
+	      return result;
+	}
+	
 	public static String[] valeurEntreeDansLeChamp(UIComponent components, String nom) {
 		UIInput uiInput = (UIInput) components.findComponent("motDePass");
 		String valeur = uiInput.getLocalValue() == null ? "" : uiInput.getLocalValue().toString();
